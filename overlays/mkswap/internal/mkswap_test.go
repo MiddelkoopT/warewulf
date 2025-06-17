2beff0bff100f611ae950bacb2930b6697e69992
Jonathon Anderson <janderson@ciq.com>
2025-06-17 10:29:19 -0600

    Support native filesystem configuration in mkswap
    Signed-off-by: Jonathon Anderson <janderson@ciq.com>

--- overlays/mkswap/internal/mkswap_test.go
+++ overlays/mkswap/internal/mkswap_test.go
@@ -59,7 +59,7 @@ fi
 `,
 		},
 
-		"mkswap:20-mkswap.sh.ww (populated)": {
+		"mkswap:20-mkswap.sh.ww (resource)": {
 			args: []string{"--quiet=true", "--render=node1", "mkswap", "warewulf/wwinit.d/20-mkswap.sh.ww"},
 			nodesConf: `
 nodes:
@@ -112,7 +112,7 @@ fi
 `,
 		},
 
-		"mkswap:20-mkswap.sh.ww (overwrite)": {
+		"mkswap:20-mkswap.sh.ww (resource overwrite)": {
 			args: []string{"--quiet=true", "--render=node1", "mkswap", "warewulf/wwinit.d/20-mkswap.sh.ww"},
 			nodesConf: `
 nodes:
@@ -121,6 +121,129 @@ nodes:
       mkswap:
         - device: /dev/disk/by-partlabel/swap
           overwrite: true`,
+			output: `#!/bin/sh
+
+PATH=$PATH:/sbin:/usr/sbin:/bin:/usr/bin
+
+if ! command -v info >/dev/null; then
+    info() {
+        printf '%s\n' "$*"
+    }
+fi
+
+if ! command -v die >/dev/null; then
+    die() {
+        printf '%s\n' "$*" >&2
+        exit 1
+    }
+fi
+
+already_formatted() {
+    if ! command -v wipefs >/dev/null ; then
+        info "warewulf: wipefs not found, cannot check if device is already formatted"
+        return 0
+    fi
+
+    if wipefs -n "${1}" &>/dev/null; then
+        info "warewulf: ${1} already formatted"
+        return 0
+    fi
+
+    return 1
+}
+
+if command -v mkswap >/dev/null; then :
+    if true || ! already_formatted /dev/disk/by-partlabel/swap; then
+        info "warewulf: mkswap: formatting /dev/disk/by-partlabel/swap"
+        mkswap  /dev/disk/by-partlabel/swap  || die "warewulf: mkswap: failed to format /dev/disk/by-partlabel/swap"
+    else
+        info "warewulf: mkswap: skipping /dev/disk/by-partlabel/swap"
+        continue
+    fi
+else
+    info "warewulf: mkswap not found"
+fi
+`,
+		},
+
+		"mkswap:20-mkswap.sh.ww (native)": {
+			args: []string{"--quiet=true", "--render=node1", "mkswap", "warewulf/wwinit.d/20-mkswap.sh.ww"},
+			nodesConf: `
+nodes:
+  node1:
+    filesystems:
+      /dev/disk/by-partlabel/rootfs:
+        format: ext4
+        path: /
+      /dev/disk/by-partlabel/scratch:
+        format: ext4
+        path: /scratch
+        wipe_filesystem: true
+      /dev/disk/by-partlabel/swap:
+        format: swap
+        path: swap`,
+			output: `#!/bin/sh
+
+PATH=$PATH:/sbin:/usr/sbin:/bin:/usr/bin
+
+if ! command -v info >/dev/null; then
+    info() {
+        printf '%s\n' "$*"
+    }
+fi
+
+if ! command -v die >/dev/null; then
+    die() {
+        printf '%s\n' "$*" >&2
+        exit 1
+    }
+fi
+
+already_formatted() {
+    if ! command -v wipefs >/dev/null ; then
+        info "warewulf: wipefs not found, cannot check if device is already formatted"
+        return 0
+    fi
+
+    if wipefs -n "${1}" &>/dev/null; then
+        info "warewulf: ${1} already formatted"
+        return 0
+    fi
+
+    return 1
+}
+
+if command -v mkswap >/dev/null; then :
+    if false || ! already_formatted /dev/disk/by-partlabel/swap; then
+        info "warewulf: mkswap: formatting /dev/disk/by-partlabel/swap"
+        mkswap  /dev/disk/by-partlabel/swap  || die "warewulf: mkswap: failed to format /dev/disk/by-partlabel/swap"
+    else
+        info "warewulf: mkswap: skipping /dev/disk/by-partlabel/swap"
+        continue
+    fi
+else
+    info "warewulf: mkswap not found"
+fi
+`,
+		},
+
+		"mkswap:20-mkswap.sh.ww (native overwrite)": {
+			args: []string{"--quiet=true", "--render=node1", "mkswap", "warewulf/wwinit.d/20-mkswap.sh.ww"},
+			nodesConf: `
+nodes:
+  node1:
+    filesystems:
+      /dev/disk/by-partlabel/rootfs:
+        format: ext4
+        path: /
+      /dev/disk/by-partlabel/scratch:
+        format: ext4
+        path: /scratch
+        wipe_filesystem: true
+      /dev/disk/by-partlabel/swap:
+        format: swap
+        path: swap
+        wipe_filesystem: true`,
 			output: `#!/bin/sh
 
 PATH=$PATH:/sbin:/usr/sbin:/bin:/usr/bin
